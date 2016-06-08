ee35fe43967510d80d5dc97c0181756a5af9053d
shao313 <shaozhu@chicagobooth.edu>
2016-06-07 22:28:11 -0500

    Second

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,5 @@
 class Company < ActiveRecord::Base
   has_many :complaints, :class_name => "Complaint", :foreign_key => "company_id"
   has_many :haters, :through => :sucks, :source => :user
-  validates :company_name, :presence => true
+  validates :company_name, :presence => true, :uniqueness => true 
 end
